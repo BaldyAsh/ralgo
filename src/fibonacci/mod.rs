8ba3b951f5d3137df84ae21568d53c29b5386c9e
AntonGrigorevSecurrency <anton.grigorev@securrency.com>
2021-02-05 19:35:13 +0400

    Fibonacci simple algo

---
+++ src/fibonacci/mod.rs
@@ -0,0 +1,69 @@
+pub fn calc_fib(num: u64) -> u64 {
+    if num <= 1 { return num; }
+
+    let mut a = 0;
+    let mut b = 1;
+    let mut c;
+
+    for _ in 2 ..= num {
+        c = b;
+        b = a + b;
+        a = c;
+    }
+
+    return b;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::calc_fib;
+
+    #[test]
+    fn test0() {
+        let result = calc_fib(0);
+
+        assert_eq!(result, 0);
+    }
+
+    #[test]
+    fn test1() {
+        let result = calc_fib(1);
+
+        assert_eq!(result, 1);
+    }
+
+    #[test]
+    fn test2() {
+        let result = calc_fib(2);
+
+        assert_eq!(result, 1);
+    }
+
+    #[test]
+    fn test3() {
+        let result = calc_fib(3);
+
+        assert_eq!(result, 2);
+    }
+
+    #[test]
+    fn test4() {
+        let result = calc_fib(4);
+
+        assert_eq!(result, 3);
+    }
+
+    #[test]
+    fn test5() {
+        let result = calc_fib(5);
+
+        assert_eq!(result, 5);
+    }
+
+    #[test]
+    fn test6() {
+        let result = calc_fib(15);
+
+        assert_eq!(result, 610);
+    }
+}
